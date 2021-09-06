948e20da8db6e065d01f7c10cbc25eb335cb7043
J Yoo <ujey02@gmail.com>
2021-09-06 20:06:32 +0900

    309. Best Time to Buy and Sell Stock with Cooldown
    309. Best Time to Buy and Sell Stock with Cooldown

--- BestTimeToBuyAndSellStockWithCooldown.py
+++ BestTimeToBuyAndSellStockWithCooldown.py
@@ -1,24 +1,23 @@
 # 99.30 / 63.70
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        if len(prices) == 1:
+        n = len(prices)
+        if n == 1:
             return 0
-        elif len(prices) == 2:
+        elif n == 2:
             return max(prices[1]-prices[0], 0)
-        
-        profits = [-prices[0],0,-1001,-1001] #[[buy, sell, buy-cooldown, sell-cooldown]]
+                
+        profits = [-prices[0],0,0] #[[buy, sell, cooldown]]
         
         '''
-        buy = can only be done when previous action is sell-cooldown,
-        sell = can only be done when previous action is buy or buy-cooldown
+        buy = can only be done after a cooldown before,
+        sell = can only be done when something is already bought
         '''
         
-        for i in range(1,len(prices)):
+        for i in range(1, n):
             price = prices[i]
-            b,s,bc,sc = profits
-            bb = max(b, bc)
+            b,s,c = profits
             
-            profits = [max(-price,sc-price), bb+price, bb, max(s,sc)]
-        
-        # print (profits)
-        return max(profits[1], profits[3])
+            profits = [max(b,c-price), b+price, max(s,c)]
+            
+        return max(profits[1], profits[2])
